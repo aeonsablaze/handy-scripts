069cb12b83de5c32473bf92a4568d4decc9d7d45
Ian Blais <me@ianblais.com>
2015-04-21 03:02:32 -0400

    better commenting in webspinner

--- webspinner.sh
+++ webspinner.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# Script to swiftly toggle back and forth between the Apache and Nginx webserver.
+# Script to swiftly toggle back and forth between the Apache and Nginx webservers.
 
+#grab current status of services
 nraw=$(service nginx status)
 araw=$(service apache2 status)
 
-#echo "$nraw"
-#echo "$araw"
-
+#parse nginx status
 if [[ "$nraw" == *"is running" ]]
 then
 	ns=1
@@ -17,6 +16,7 @@ else
 	echo "Could not determine nginx status. Exiting."
 fi
 
+#parse apache status
 if [[ "$araw" == *"is running" ]]
 then
         as=1
@@ -27,27 +27,29 @@ else
         echo "Could not determine apache2 status. Exiting."
 fi
 
-#echo $ns
-#echo $as
-
 if [ $as == 1 ] && [ $ns == 0 ]
 	then
+	#If apache running switch to nginx.
 	echo "Stopping Apache and Starting Nginx."
 	sudo service apache2 stop >/dev/null
 	sudo service nginx start >/dev/null
 	echo "Done."
 elif [ $as == 0 ] && [ $ns == 1 ]
 	then
+	#If nginx running, switch to apache.
 	echo "Stopping Nginx and Starting Apache."
 	sudo service nginx stop >/dev/null
 	sudo service apache2 start > /dev/null
 	echo "Done."
 elif [ $as == 0 ] && [ $ns == 0 ]
 	then
+	#if neither running, cancel.
 	echo "Sorry Chief, but neither Apache nor Nginx are running."
 elif [ $as == 1 ] && [ $ns == 1 ]
 	then
+	#if both running, cancel.
 	echo "Whoa, both Apache and Nginx are running!? You should, like, fix that."
 else
-	echo "... Something has gone horribly wrong dave..."
+	#??? how would this even happen?
+	echo "... Something has gone horribly wrong... I'm frightened..."
 fi
